419b8c036dd5582798d1b69ff410b7f12351de02
SENITH <SENITH@DESKTOP-NJ07VAK>
2021-06-08 00:58:18 +0530

    Create Music_Mart Abstract class

--- src/main/java/Model/MusicMart.java
+++ src/main/java/Model/MusicMart.java
@@ -1,6 +1,82 @@
-
 package Model;
 
-public abstract class MusicMart {
+public  class MusicMart {
+
+    private String AdminName ;
+    private String AdminPassword ;
+    public MusicMart(String AdminName,String AdminPassword)
+    {
+        this.AdminName=AdminName;
+        this.AdminPassword=AdminPassword;
+    }
+    public MusicMart()
+    {
+        this.AdminName=null;
+        this.AdminPassword=null;
+    }
+
+    public class CustomerAccount {
+
+        private int CusID;
+        private String CusName;
+        private String CusPassword;
+        private String CusEmail;
+        private String CusMobile;
+
+        public CustomerAccount(int CusID, String CusName, String CusPassword, String CusEmail, String CusMobile) {
+            this.CusID = CusID;
+            this.CusName = CusName;
+            this.CusPassword = CusPassword;
+            this.CusEmail = CusEmail;
+            this.CusMobile = CusMobile;
+        }
+
+        public CustomerAccount() {
+            this.CusID = 0;
+            this.CusName = null;
+            this.CusPassword = null;
+            this.CusEmail = null;
+            this.CusMobile = null;
+
+        }
+    }
+
+    public class RepairistAcc {
+
+        private int RepID;
+        private String RepName;
+        private String RepPassword;
+        public RepairistAcc(int RepID, String RepName,String RepPassword)
+        {
+            this.RepID = RepID;
+            this.RepName = RepName;
+            this.RepPassword =RepPassword;
+        }
+        public RepairistAcc()
+        {
+            this.RepID = 0;
+            this.RepName = null;
+            this.RepPassword = null;
+        }
+    }
     
+    public class SupplierAcc{
+        private int SupID;
+        private String SupName;
+        private String SupPassword;
+        public SupplierAcc(int SupID, String SupName,String SupPassword)
+        {
+            this.SupID = SupID;
+            this.SupName = SupName;
+            this.SupPassword =SupPassword;
+        }
+        public SupplierAcc()
+        {
+            this.SupID = 0;
+            this.SupName = null;
+            this.SupPassword = null;
+        }
+        
+    }
+
 }
---
+++ src/main/java/Model/OrderInstrument.java
@@ -0,0 +1,29 @@
+package Model;
+
+public class OrderInstrument extends MusicMart {
+
+    private int InsID;
+    private String InsName;
+    private float InsPrice;
+    private int InsQuantity;
+
+    public OrderInstrument(int InsID, String InsName, float InsPrice, int InsQuantity) {
+        this.InsID = InsID;
+        this.InsName = InsName;
+        this.InsPrice = InsPrice;
+        this.InsQuantity = InsQuantity;
+    }
+
+    public OrderInstrument() {
+        this.InsID = 0;
+        this.InsName = null;
+        this.InsPrice = 0.0f;
+        this.InsQuantity = 0;
+    }
+
+    public float CalTotal() {
+        float InsTotal = InsQuantity * InsPrice;
+        return InsTotal;
+    }
+    
+}
